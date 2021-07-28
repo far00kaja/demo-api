fd1520e3b62ebf0b6098e0d43b52b7c2ac35e25d
Dicky Darmawan <farookaja@gmail.com>
2021-07-28 09:21:06 +0700

    FIRST, create CRUD data java spring boot rest API

---
+++ src/main/java/com/domain/services/ProductService.java
@@ -0,0 +1,46 @@
+package com.domain.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import com.domain.models.entities.Product;
+import com.domain.models.repos.ProductRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product save(Product product){
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id){
+        Optional<Product> temp = productRepo.findById(id);
+        if (!temp.isPresent()){
+            return null;
+        }
+        return productRepo.findById(id).get();
+    }
+
+    public Iterable<Product> findAll(){
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id){
+        productRepo.deleteById(id);
+    }
+    
+    public List<Product> findByNameContains(String name){
+        return productRepo.findByNameContains(name);
+    }
+
+    
+}
