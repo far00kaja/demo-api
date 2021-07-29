586f1b114edc472e88114c1f6b9138829fd31587
Dicky Darmawan <farookaja@gmail.com>
2021-07-29 18:08:06 +0700

    learning JsonIdentityInfo

--- src/main/java/com/domain/controllers/SupplierController.java
+++ src/main/java/com/domain/controllers/SupplierController.java
@@ -1,21 +1,19 @@
 package com.domain.controllers;
 
-
 import javax.validation.Valid;
 
 import com.domain.dto.ResponseData;
-import com.domain.models.entities.Product;
+import com.domain.dto.SupplierData;
 import com.domain.models.entities.Supplier;
-import com.domain.services.ProductService;
+import com.domain.services.SupplierService;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,66 +21,70 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/products")
-public class ProductController {
+@RequestMapping("api/suppliers")
+public class SupplierController {
     
     @Autowired
-    private ProductService productService;
+    private SupplierService supplierService;
+    
+    @Autowired
+    private ModelMapper modelMapper;
 
     @PostMapping
-    public ResponseEntity<ResponseData<Product>> create(@Valid @RequestBody Product product, Errors errors){
-        
-            ResponseData<Product> responseData = new ResponseData<>();
-        if (errors.hasErrors()){
-            for (ObjectError error : errors.getAllErrors()){
-                responseData.getMessages().add(error.getDefaultMessage());
-            }  
-            responseData.setStatus(false);
-            responseData.setPayload(null);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
-        }
-        responseData.setStatus(true);
-        responseData.setPayload(productService.save(product));
-        return ResponseEntity.ok(responseData);
-    }
+    public ResponseEntity<ResponseData<Supplier>> create(@Valid @RequestBody SupplierData supplierData, Errors Errors){
+        ResponseData<Supplier> responseData = new ResponseData<>();
 
-    @GetMapping
-    public Iterable<Product> findAll(){
-        return productService.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public Product findOne(@PathVariable("id") Long id){
-        return productService.findOne(id);
-    }
-
-    @PutMapping
-    public ResponseEntity<ResponseData<Product>> update(@Valid @RequestBody Product product, Errors errors){
-        ResponseData<Product> responseData = new ResponseData<>();
-
-        if (errors.hasErrors()){
-            for(ObjectError error : errors.getAllErrors()){
+        if (Errors.hasErrors()){
+            for(ObjectError error: Errors.getAllErrors()){
                 responseData.getMessages().add(error.getDefaultMessage());
             }
             responseData.setStatus(false);
             responseData.setPayload(null);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
         }
+
+        //sebelum modelMapper
+        // Supplier supplier =  new Supplier();
+        // supplier.setName(supplierData.getName());
+        // supplier.setAddress(supplierData.getAddress());
+        // supplier.setEmail(supplierData.getEmail());
+
+        //sesudah pakai modelMapper
+        Supplier supplier = modelMapper.map(supplierData, Supplier.class);
         responseData.setStatus(true);
-        responseData.setPayload(productService.save(product));
+        responseData.setPayload(supplierService.save(supplier));
         return ResponseEntity.ok(responseData);
+
     }
 
-    @DeleteMapping("/{id}")
-    public void removeOne(@PathVariable("id") Long id){
-        productService.removeOne(id);
+    @GetMapping
+    public Iterable<Supplier> findAll(){
+        return supplierService.findAll();
     }
 
-    @PostMapping("/{id}")
-    public void addSupplier(@RequestBody Supplier supplier, @PathVariable("id") Long productId){
-        productService.addSupplier(supplier, productId);
+    @GetMapping("/{id}")
+    public Supplier findOne(Long id){
+        return supplierService.findOne(id);
     }
 
-    
+    @PutMapping
+    public ResponseEntity<ResponseData<Supplier>> update(@Valid @RequestBody SupplierData supplierData, Errors Errors){
+        ResponseData<Supplier> responseData = new ResponseData<>();
+
+        if (Errors.hasErrors()){
+            for(ObjectError error: Errors.getAllErrors()){
+                responseData.getMessages().add(error.getDefaultMessage());
+            }
+            responseData.setStatus(false);
+            responseData.setPayload(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+
+        Supplier supplier = modelMapper.map(supplierData, Supplier.class);
+        responseData.setStatus(true);
+        responseData.setPayload(supplierService.save(supplier));
+        return ResponseEntity.ok(responseData);
+
+    }
 
 }
